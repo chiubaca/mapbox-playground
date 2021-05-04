1fa6fcde0436c6f050b81b8f8a6f2083b2eb5697
Alex Chiu <alexc@neverbland.com>
2021-05-04 08:50:26 +0100

    2nd demo map container using route tileset

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./App.css";
@@ -6,12 +6,14 @@ import "./App.css";
 import * as route from "../data-scripts/output/combined.json";
 
 function App() {
-  const mapContainer = useRef<HTMLElement>(null!);
+  const mapContainer1 = useRef<HTMLElement>(null!);
+  const mapContainer2 = useRef<HTMLElement>(null!);
   mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY as string;
 
+  //map 1
   useEffect(() => {
-    var map = new mapboxgl.Map({
-      container: mapContainer.current,
+    const map = new mapboxgl.Map({
+      container: mapContainer1.current,
       style: "mapbox://styles/mapbox/streets-v11",
       zoom: 5,
       center: [4.6865447, 47.6878221],
@@ -71,14 +73,54 @@ function App() {
     return () => map.remove();
   }, []);
 
+  //map 2
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: mapContainer2.current,
+      style: "mapbox://styles/chiubaca/cko8z52hk19os17o9epehszt0",
+      zoom: 5,
+      center: [4.6865447, 47.6878221],
+      pitch: 50,
+      bearing: 0,
+    });
+
+    map.on("load", function () {
+      map.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        tileSize: 512,
+        maxzoom: 14,
+      });
+
+      // add the DEM source as a terrain layer with exaggerated height
+      map.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+
+      // add a sky layer that will show when the map is highly pitched
+      map.addLayer({
+        id: "sky",
+        type: "sky",
+        paint: {
+          "sky-type": "atmosphere",
+          "sky-atmosphere-sun": [0.0, 0.0],
+          "sky-atmosphere-sun-intensity": 15,
+        },
+      });
+    });
+
+    return () => map.remove();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Hello Vite, React, TS &amp; Mapbox!</p>
       </header>
-      <div className="map-container" ref={mapContainer}>
-        Map Container
-      </div>
+
+      <h2>Map with GeoJson</h2>
+      <div className="map-container" ref={mapContainer1}></div>
+
+      <h2>Map with mapbox dataset</h2>
+      <div className="map-container" ref={mapContainer2}></div>
     </div>
   );
 }
