da265054d13656950aace1343263caddcb19595a
Alex Chiu <alexc@neverbland.com>
2021-05-08 12:52:39 +0100

    pan to birds eye view animation

--- src/maps/mfm/MFMMap.tsx
+++ src/maps/mfm/MFMMap.tsx
@@ -1,37 +1,35 @@
 import React, { useEffect, useRef, useState } from "react";
-import * as turf from "@turf/turf";
+// import * as turf from "@turf/turf";
 import mapboxgl from "mapbox-gl";
 
-import * as path from "../../../data-scripts/output/combined.json";
-
-mapboxgl.accessToken = import.meta.env.VITE_NB_MAPBOX_KEY as string;
+mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_KEY as string;
 
 export default function MFMMap() {
   let map: mapboxgl.Map;
   const mapContainer = useRef<HTMLDivElement>(null!);
-  const [rotate, setRotate] = useState(true);
 
-  //map 3
+  const requestRef = React.useRef(0);
+
+  const rotateCamera = (timestamp: number) => {
+    // clamp the rotation between 0 -360 degrees
+    // Divide timestamp by 500 to slow rotation to ~10 degrees / sec
+    map.rotateTo((timestamp / 500) % 360, { duration: 0 });
+
+    // console.log("rotate?", rotate);
+
+    // Request the next frame of the animation.
+    requestRef.current = requestAnimationFrame(rotateCamera);
+  };
+
   useEffect(() => {
     map = new mapboxgl.Map({
       container: mapContainer.current,
-      style: "mapbox://styles/neverbland-studio/cko64s47z2tzq19pqrfw1pws9",
+      style: "mapbox://styles/chiubaca/ckofkafej523q17ryvp4au82j",
       zoom: 12,
       center: [84.18751464765715, 28.64795221351989],
       pitch: 60,
       bearing: 0,
     });
-
-    function rotateCamera(timestamp: number) {
-      // clamp the rotation between 0 -360 degrees
-      // Divide timestamp by 500 to slow rotation to ~10 degrees / sec
-      map.rotateTo((timestamp / 500) % 360, { duration: 0 });
-
-      if (rotate) {
-        // Request the next frame of the animation.
-        requestAnimationFrame(rotateCamera);
-      }
-    }
 
     map.on("load", function () {
       map.addSource("mapbox-dem", {
@@ -54,17 +52,56 @@ export default function MFMMap() {
           "sky-atmosphere-sun-intensity": 15,
         },
       });
-
-      rotateCamera(0);
     });
+
+    // start rotate animation
+    requestRef.current = requestAnimationFrame(rotateCamera);
 
     return () => map.remove();
   }, []);
 
+  const viewMapHandler = () => {
+    cancelAnimationFrame(requestRef.current);
+    map.flyTo({
+      center: [40.467, 35.703],
+      zoom: 3.13,
+      speed: 5,
+      bearing: 0,
+      pitch: 0,
+      curve: 1,
+    });
+  };
+
   return (
-    <div className="mfm-map" ref={mapContainer}>
-      <div className="overlay-canvas">
-        <button className="test-button">Test</button>
+    <div className="mfm--map" ref={mapContainer}>
+      <div className="mfm--overlay">
+        <p className="mfm--hero-text">Track our Progress</p>
+
+        <svg
+          onClick={viewMapHandler}
+          className="mfm--view-map "
+          width="48"
+          height="50"
+          viewBox="0 0 58 60"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g>
+            <path
+              d="M57.06 39.3V20.7L46.14 5.7L28.56 0L10.92 5.7L0 20.7V39.3L10.92 54.3L28.56 60L46.14 54.3L57.06 39.3Z"
+              fill="#E4503A"
+            />
+            <text
+              x="14"
+              y="35"
+              font-family="Verdana"
+              font-size="12"
+              fill="white"
+            >
+              View
+            </text>
+          </g>
+        </svg>
       </div>
     </div>
   );
