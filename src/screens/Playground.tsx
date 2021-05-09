291fbae2282ec05e1e3c33b409e5bfb0e206ca54
Alex Chiu <alexc@neverbland.com>
2021-05-09 11:17:33 +0100

    link to mfm demo

--- src/screens/Playground.tsx
+++ src/screens/Playground.tsx
@@ -1,16 +1,22 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import Map1 from "../maps/playground/Map1";
 import Map2 from "../maps/playground/Map2";
 import Map3 from "../maps/playground/Map3";
 
 export function Playground() {
+  let history = useHistory();
+  function linkHandler() {
+    history.push("/mfm-demo");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>NB̶ Mapbox Playground</h1>
       </header>
       <div className="demo-container">
+        <a onClick={linkHandler}>mfm demo</a>
         <h2>Route Overview</h2>
         <Map1 />
         <h2>3D terrain</h2>
